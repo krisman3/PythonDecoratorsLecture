ea5cde6e61ee2ffcc63c1809eecb6d112ae29892
KIliev <kriskokz@gmail.com>
2024-05-21 15:00:52 +0300

    more efficient

--- main.py
+++ main.py
@@ -68,12 +68,12 @@
 # func_needs_decorator()
 
 def create_cubes(n):
-    result = []
-    for x in range(n):
-        result.append(x**3)
-    return result
+
+   for x in range(n):
+       yield x**3
 
 print(create_cubes(10))
 
 for x in create_cubes(10):
     print(x)
+
